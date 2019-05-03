3f41eb0416a4fdaa7bfae8696249103903dc6497
thiefjim <thiefjim@gmail.com>
2019-05-03 14:13:04 +0800

    Ch6 add comment

--- Ch-06/alpha-bot-login-v0/index.js
+++ Ch-06/alpha-bot-login-v0/index.js
@@ -2,57 +2,62 @@
 const express = require('express');
 const line_login = require('./line-login');
 const path = require('path');
-const session = require("express-session");
+const session = require('express-session');
 
 //設定session參數
 const session_options = {
   secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
   resave: false,
   saveUninitialized: false
-}
+};
 //設定LINE Login參數
 const loginConfig = {
   channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
   channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
   callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
-  scope: "openid profile",
-  prompt: "consent",
-  bot_prompt: "normal"
-}
-console.log(process.env.HEROKU_APP_NAME);
-const lineLogin = new line_login(loginConfig)
+  scope: 'openid profile',
+  prompt: 'consent',
+  bot_prompt: 'normal'
+};
+
+const lineLogin = new line_login(loginConfig);
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(session(session_options));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));  //設定Template目錄，畫面樣板
+app.set('view engine', 'pug');  //設定Template引擎，用於產生畫面
+app.use(session(session_options));  //設定使用Session
+app.use(express.static(path.join(__dirname, 'public'))); //設定可以取得的檔案
 
-app.get("/", (req, res) => {
+//自訂的畫面路由
+app.get('/', (req, res) => {    
   if (req.session.authPass) {
     const profile = req.session.profile;
-    res.render('success', profile)
+    res.render('success', profile); //自訂成功登入頁面
   } else if (req.session.errMsg) {
-    res.render('login', {
+    res.render('login', {  //自訂尚未登入頁面，顯示錯誤訊息
       ErrMsg: req.session.errMsg
-    })
+    });
   } else {
-    res.render('login')
+    res.render('login');  //自訂尚未登入頁面，沒有錯誤訊息
   }
-})
+});
 
-app.get("/auth/line", lineLogin.authDirect());
-app.get("/auth/line/cb", lineLogin.authcb(
+//LINE Login相關的API
+app.get('/auth/line', lineLogin.authDirect()); //產生跳轉到LINE的登入網址
+app.get('/auth/line/cb', lineLogin.authcb( //從LINE登入後接收訊息
   (req, res, token) => {
     req.session.authPass = true;
     req.session.profile = token.id_token;
+    res.redirect('/');
   }, (req, res, next, error) => {
     req.session.authPass = false;
-    req.session.errMsg = error.message
+    req.session.errMsg = error.message;
     res.redirect('/');
   }
 ));
-app.get("/auth/line/logout", (req, res) => {
+
+//清除session API
+app.get('/auth/line/logout', (req, res) => { //登出帳號清除session
   req.session.destroy();
   res.redirect('/');
 });
--- Ch-06/alpha-bot-login-v0/line-login.js
+++ Ch-06/alpha-bot-login-v0/line-login.js
@@ -1,101 +1,108 @@
-const request = require("request");
-const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-let Promise = require("bluebird");
+const request = require('request');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const Promise = require('bluebird');
+
 Promise.promisifyAll(request);
 
 class LineLogin {
-    constructor(opts){
-        const required_params = ["channel_id", "channel_secret", "callback_url"];
-        required_params.map((params) => {
-            if (!opts[params]){
-                throw new Error(`${params} missing.`);
+  constructor(opts) { //預設參數
+    const required_params = ['channel_id', 'channel_secret', 'callback_url'];
+    required_params.map((params) => {
+      if (!opts[params]) {
+        throw new Error(`${params} missing.`);
+      }
+    });
+    this.channel_id = opts.channel_id;
+    this.channel_secret = opts.channel_secret;
+    this.callback_url = opts.callback_url;
+    this.scope = opts.scope || 'profile openid';
+    this.prompt = opts.prompt;
+    this.bot_prompt = opts.bot_prompt || 'normal';
+    this.baseURL = 'https://access.line.me/oauth2/';
+  }
+
+  authDirect() { //產生使用LINE Loign的網址
+    return (req, res) => {
+      const state = req.session.line_login_state = LineLogin._random();
+      const nonce = req.session.line_login_nonce = LineLogin._random();
+      const url = this.secure_auth_url(state, nonce); //取得與第三方應用的相關參數的網址
+      return res.redirect(url);
+    };
+  }
+
+  secure_auth_url(state, nonce) { //產生與第三方應用的相關參數的網址
+    const client_id = encodeURIComponent(this.channel_id);
+    const redirect_uri = encodeURIComponent(this.callback_url);
+    const scope = encodeURIComponent(this.scope);
+    const bot_prompt = encodeURIComponent(this.bot_prompt);
+    let url = `${this.baseURL}v2.1/authorize?response_type=code&`+
+              `client_id=${client_id}&redirect_uri=${redirect_uri}`+
+              `&scope=${scope}&bot_prompt=${bot_prompt}&state=${state}`;
+    if (this.prompt) url += `&prompt=${encodeURIComponent(this.prompt)}`;
+    if (nonce) url += `&nonce=${encodeURIComponent(nonce)}`;
+    return url;
+  }
+
+  authcb(succ, fail) {  //處理LINE Server的跳轉授權，並取得Aceesk Token
+    return (req, res, next) => {
+      const { code } = req.query;
+      const { state } = req.query;
+      if (req.session.line_login_state !== state) { //驗證是否為相同使用者的瀏覽器
+        return fail(req, res, next, new Error('Authorization failed. State does not match.'));
+      }
+      this.issue_line_access_token(code).then((token_response) => { //取得Aceesk Token
+        if (token_response.id_token) {
+          let decoded_id_token;
+          try {
+            decoded_id_token = jwt.verify( //JWT 解碼與驗證
+              token_response.id_token,
+              this.channel_secret,
+              {
+                audience: this.channel_id,
+                issuer: 'https://access.line.me',
+                algorithms: ['HS256'],
+              },
+            );
+            if (decoded_id_token.nonce !== req.session.line_login_nonce) { //驗證JWT是否有被修改過
+              throw new Error('nonce not match.');
             }
-        })
-        this.channel_id = opts.channel_id;
-        this.channel_secret = opts.channel_secret;
-        this.callback_url = opts.callback_url;
-        this.scope = opts.scope || "profile openid";
-        this.prompt = opts.prompt;
-        this.bot_prompt = opts.bot_prompt || "normal";
-        this.baseURL = 'https://access.line.me/oauth2/';
-    }
-    authDirect(){
-        return (req, res) => {
-            let state = req.session.line_login_state = LineLogin._random();
-            let nonce = req.session.line_login_nonce = LineLogin._random();
-            let url = this.secure_auth_url(state, nonce);
-            return res.redirect(url);
+            token_response.id_token = decoded_id_token; //
+          } catch (exception) {
+            fail(new Error('Verification of id token failed.'));
+          }
         }
-    }
-    secure_auth_url(state, nonce){
-        const client_id = encodeURIComponent(this.channel_id);
-        const redirect_uri = encodeURIComponent(this.callback_url);
-        const scope = encodeURIComponent(this.scope);
-        const prompt = encodeURIComponent(this.prompt);
-        const bot_prompt = encodeURIComponent(this.bot_prompt);
-        let url = `${this.baseURL}v2.1/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&bot_prompt=${bot_prompt}&state=${state}`;
-        if (this.prompt) url += `&prompt=${encodeURIComponent(this.prompt)}`;
-        if (nonce) url += `&nonce=${encodeURIComponent(nonce)}`;
-        return url
-    }
-    authcb(succ,fail){
-        return (req,res,next) =>{
-            const code = req.query.code;
-            const state = req.query.state;
-            if (req.session.line_login_state!==state){
-                return fail(req,res,next,new Error("Authorization failed. State does not match."));
-            }
-            this.issue_line_access_token(code).then((token_response) => {
-                if (token_response.id_token){
-                    let decoded_id_token;
-                    try {
-                        decoded_id_token = jwt.verify(
-                            token_response.id_token,
-                            this.channel_secret,
-                            {
-                                audience: this.channel_id,
-                                issuer: "https://access.line.me",
-                                algorithms: ["HS256"]
-                            }
-                        );
-                        if (decoded_id_token.nonce!==req.session.line_login_nonce){
-                            throw new Error("nonce not match.");
-                        }
-                        token_response.id_token = decoded_id_token;
-                    } catch(exception) {
-                        fail(new Error("Verification of id token failed."));
-                    }
-                }
-                delete req.session.line_login_state;
-                delete req.session.line_login_nonce;                                
-                succ(req, res, token_response);
-            }).catch((error) => {
-                fail(req,res,next,error);
-            });
-        }
-    }
-    issue_line_access_token(code){
-        const url = `https://api.line.me/oauth2/v2.1/token`;
-        const form = {
-            grant_type: "authorization_code",
-            code: code,
-            redirect_uri: this.callback_url,
-            client_id: this.channel_id,
-            client_secret: this.channel_secret
-        }
-        return request.postAsync({
-            url: url,
-            form: form
-        }).then((response) => {
-            if (response.statusCode == 200){
-                return JSON.parse(response.body);
-            }
-            return Promise.reject(new Error(response.statusMessage));
-        });
-    }
-    static _random(){
-        return crypto.randomBytes(20).toString('hex');
-    }
+        delete req.session.line_login_state;
+        delete req.session.line_login_nonce;
+        succ(req, res, token_response); //將Acess Token傳給succ的函數
+      }).catch((error) => {
+        fail(req, res, next, error); //將error傳給傳給fail的函數
+      });
+    };
+  }
+
+  issue_line_access_token(code) { //取得使用者的Access Token
+    const url = 'https://api.line.me/oauth2/v2.1/token';
+    const form = {
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: this.callback_url,
+      client_id: this.channel_id,
+      client_secret: this.channel_secret,
+    };
+    return request.postAsync({
+      url,
+      form,
+    }).then((response) => {
+      if (response.statusCode == 200) {
+        return JSON.parse(response.body);
+      }
+      return Promise.reject(new Error(response.statusMessage));
+    });
+  }
+
+  static _random() {  //產生亂數40位的16進制字串
+    return crypto.randomBytes(20).toString('hex');
+  }
 }
 module.exports = LineLogin;
